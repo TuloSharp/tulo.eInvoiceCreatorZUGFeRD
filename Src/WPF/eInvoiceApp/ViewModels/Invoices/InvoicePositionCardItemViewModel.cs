7752eb9a4cc557614bfc64accfd0eff8ea1f4a89
Marcelo Andrade <guartan.andrade.marcelo@gmail.com>
2026-03-02 22:06:00 +0100

    solve issue with select, wrong id

--- Src/WPF/eInvoiceApp/ViewModels/Invoices/InvoicePositionCardItemViewModel.cs
+++ Src/WPF/eInvoiceApp/ViewModels/Invoices/InvoicePositionCardItemViewModel.cs
@@ -4,64 +4,62 @@ using tulo.CommonMVVM.Commands;
 using tulo.CommonMVVM.ViewModels;
 using tulo.eInvoice.eInvoiceApp.DTOs;
 
-namespace tulo.eInvoice.eInvoiceApp.ViewModels.Invoices
+namespace tulo.eInvoice.eInvoiceApp.ViewModels.Invoices;
+
+public class InvoicePositionCardItemViewModel : BaseViewModel
 {
-    public class InvoicePositionCardItemViewModel : BaseViewModel
+    public InvoicePositionDetailsDTO InvoicePositionDetails { get; private set; }
+
+    public Guid InvoicePositionId => InvoicePositionDetails.Id;
+    public int InvoicePositionNr => InvoicePositionDetails.InvoicePositionNr;
+    public string InvoicePositionDescription => InvoicePositionDetails.InvoicePositionDescription;
+    public string InvoicePositionProductDescription => InvoicePositionDetails.InvoicePositionProductDescription;
+    public string InvoicePositionItemNr => InvoicePositionDetails.InvoicePositionItemNr;
+    public string InvoicePositionEan => InvoicePositionDetails.InvoicePositionEan;
+
+    public decimal InvoicePositionQuantity => InvoicePositionDetails.InvoicePositionQuantity;
+    public string InvoicePositionUnit => InvoicePositionDetails.InvoicePostionUnit;
+
+    public decimal InvoicePositionUnitPrice => InvoicePositionDetails.InvoicePositionUnitPrice;
+
+    public decimal InvoicePositionVatRate => InvoicePositionDetails.InvoicePositionVatRate;
+
+    public decimal InvoicePositionNetAmount => InvoicePositionDetails.InvoicePositionNetAmount;
+    public decimal InvoicePositionGrossAmount => InvoicePositionDetails.InvoicePositionGrossAmount;
+
+    public string InvoicePositionDiscountReason => InvoicePositionDetails.InvoicePositionDiscountReason;
+
+    public decimal InvoicePositionDiscountNetAmount => InvoicePositionDetails.InvoicePositionDiscountNetAmount;
+    public decimal InvoicePositionNetAmountAfterDiscount => (decimal)InvoicePositionDetails.InvoicePositionNetAmountAfterDiscount!;
+
+    private bool _isDeleting;
+    public bool IsDeleting
     {
-        public Guid InvoicePositionId { get; set; }
+        get => _isDeleting;
+        set => SetField(ref _isDeleting, value);
+    }
 
-        public InvoicePositionDetailsDTO InvoicePositionDetails { get; private set; }
+    #region invoice position Commands
+    public ICommand OpenEditInvoicePositionViewCommand { get; }
+    public ICommand OpenDeleteInvoicePositionViewCommand { get; }
+    #endregion
 
-        public int InvoicePositionNr => InvoicePositionDetails.InvoicePositionNr;
-        public string InvoicePositionDescription => InvoicePositionDetails.InvoicePositionDescription;
-        public string InvoicePositionProductDescription => InvoicePositionDetails.InvoicePositionProductDescription;
-        public string InvoicePositionItemNr => InvoicePositionDetails.InvoicePositionItemNr;
-        public string InvoicePositionEan => InvoicePositionDetails.InvoicePositionEan;
+    public string ToolTipDiscountInfosExpander { get; set; } = string.Empty;
 
-        public decimal InvoicePositionQuantity => InvoicePositionDetails.InvoicePositionQuantity;
-        public string InvoicePositionUnit => InvoicePositionDetails.InvoicePostionUnit;
-
-        public decimal InvoicePositionUnitPrice => InvoicePositionDetails.InvoicePositionUnitPrice;
-
-        public decimal InvoicePositionVatRate => InvoicePositionDetails.InvoicePositionVatRate;
-
-        public decimal InvoicePositionNetAmount => InvoicePositionDetails.InvoicePositionNetAmount;
-        public decimal InvoicePositionGrossAmount => InvoicePositionDetails.InvoicePositionGrossAmount;
-
-        public string InvoicePositionDiscountReason => InvoicePositionDetails.InvoicePositionDiscountReason;
-
-        public decimal InvoicePositionDiscountNetAmount => InvoicePositionDetails.InvoicePositionDiscountNetAmount;
-        public decimal InvoicePositionNetAmountAfterDiscount => (decimal)InvoicePositionDetails.InvoicePositionNetAmountAfterDiscount!;
-
-        private bool _isDeleting;
-        public bool IsDeleting
-        {
-            get => _isDeleting;
-            set => SetField(ref _isDeleting, value);
-        }
+    public InvoicePositionCardItemViewModel(InvoicePositionDetailsDTO invoicePositionDetails, ICollectorCollection collectorCollection)
+    {
+        InvoicePositionDetails = invoicePositionDetails;
 
         #region invoice position Commands
-        public ICommand OpenEditInvoicePositionViewCommand { get; }
-        public ICommand OpenDeleteInvoicePositionViewCommand { get; }
+        OpenEditInvoicePositionViewCommand = new OpenModalStackCommand(collectorCollection, () => new EditInvoicePositionViewModel(collectorCollection), typeof(EditInvoicePositionViewModel));
+        OpenDeleteInvoicePositionViewCommand = new OpenModalStackCommand(collectorCollection, () => new DeleteInvoicePositionViewModel(collectorCollection), typeof(DeleteInvoicePositionViewModel));
         #endregion
 
-        public string ToolTipDiscountInfosExpander { get; set; } = string.Empty;
+        ToolTipDiscountInfosExpander = "Discount details (reason, net discount, net total after discount).";
+    }
 
-        public InvoicePositionCardItemViewModel(InvoicePositionDetailsDTO invoicePositionDetails, ICollectorCollection collectorCollection)
-        {
-            InvoicePositionDetails = invoicePositionDetails;
-
-            #region invoice position Commands
-            OpenEditInvoicePositionViewCommand = new OpenModalStackCommand(collectorCollection, () => new EditInvoicePositionViewModel(collectorCollection), typeof(EditInvoicePositionViewModel));
-            OpenDeleteInvoicePositionViewCommand = new OpenModalStackCommand(collectorCollection, () => new DeleteInvoicePositionViewModel(collectorCollection), typeof(DeleteInvoicePositionViewModel));
-            #endregion
-
-            ToolTipDiscountInfosExpander = "Discount details (reason, net discount, net total after discount).";
-        }
-
-        public void Update(InvoicePositionDetailsDTO invoicePositionDetails)
-        {
-            InvoicePositionDetails = invoicePositionDetails;
-        }
+    public void Update(InvoicePositionDetailsDTO invoicePositionDetails)
+    {
+        InvoicePositionDetails = invoicePositionDetails;
     }
 }
