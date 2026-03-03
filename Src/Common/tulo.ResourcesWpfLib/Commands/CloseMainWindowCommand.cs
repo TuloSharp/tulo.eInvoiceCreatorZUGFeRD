c22fba7ca63ef22492d7d9356a53074bb29c24bf
Marcelo Andrade <guartan.andrade.marcelo@gmail.com>
2026-03-03 21:49:47 +0100

    refactoring

--- Src/Common/tulo.ResourcesWpfLib/Commands/CloseMainWindowCommand.cs
+++ Src/Common/tulo.ResourcesWpfLib/Commands/CloseMainWindowCommand.cs
@@ -3,39 +3,38 @@ using System.Diagnostics;
 using System.Windows;
 using tulo.CommonMVVM.Commands;
 
-namespace tulo.ResourcesWpfLib.Commands
+namespace tulo.ResourcesWpfLib.Commands;
+
+public class CloseMainWindowCommand : BaseCommand
 {
-    public class CloseMainWindowCommand : BaseCommand
+    public override void Execute(object parameter)
     {
-        public override void Execute(object parameter)
+        if (parameter == null)
         {
-            if (parameter == null)
+            return;
+        }
+
+        try
+        {
+            object[] obj = (object[])parameter;
+            Window window = (Window)obj[0];
+            bool isMainWindow = false;
+            bool isModalOpen = false;
+
+            if (obj.Length > 1 && obj[1] is bool mainWindowFlag)
+                isMainWindow = mainWindowFlag;
+
+            if (obj.Length > 2 && obj[2] is bool modalFlag)
+                isModalOpen = modalFlag;
+
+            if (window != null && isMainWindow && !isModalOpen)
             {
-                return;
+                window.Close();
             }
-
-            try
-            {
-                object[] obj = (object[])parameter;
-                Window window = (Window)obj[0];
-                bool isMainWindow = false;
-                bool isModalOpen = false;
-
-                if (obj.Length > 1 && obj[1] is bool mainWindowFlag)
-                    isMainWindow = mainWindowFlag;
-
-                if (obj.Length > 2 && obj[2] is bool modalFlag)
-                    isModalOpen = modalFlag;
-
-                if (window != null && isMainWindow && !isModalOpen)
-                {
-                    window.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"error at execution: {ex.Message}");
-            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"error at execution: {ex.Message}");
         }
     }
 }
