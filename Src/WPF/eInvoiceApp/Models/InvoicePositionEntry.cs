bde1cc01f340e3e938dd77b203ef000dd924aa34
Marcelo Andrade <guartan.andrade.marcelo@gmail.com>
2026-03-03 21:41:38 +0100

    delete class

--- Src/WPF/eInvoiceApp/Models/InvoicePositionEntry.cs
+++
@@ -1,8 +0,0 @@
-﻿using tulo.eInvoice.eInvoiceApp.DTOs;
-
-namespace tulo.eInvoice.eInvoiceApp.Models;
-public class InvoicePositionEntry
-{
-    public Guid Id { get; init; }
-    public InvoicePositionDetailsDTO InvoicePositions { get; set; } = new();
-}
