fd59a1e5e6bc09aa9e7dcc570e0d75ef9db23dd2
Marcelo Andrade <guartan.andrade.marcelo@gmail.com>
2026-03-23 21:32:53 +0100

    refactroing code

--- Tests/tulo.SigningPdfA3Tests/Services/PdfSigningServiceTest.cs
+++ Tests/tulo.SigningPdfA3Tests/Services/PdfSigningServiceTest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using tulo.SigningPdfA3.Services;
+﻿using tulo.SigningPdfA3.Services;
 
 namespace tulo.SigningPdfA3Tests.Services;
 
